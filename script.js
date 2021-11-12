21945488115faa9cf497544bd7da1832f7635c64
Simon Tirant <simon.tirant@gmail.com>
2021-11-12 18:42:53 +0100

    components added + navigation effect fixed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const rootFontSize = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('font-size'));
 const header = document.querySelector('header');
 const navLinks = document.querySelectorAll(".navbar .nav-link");
-const heroImg = document.querySelector('.hero-img');
+const heroImg = document.querySelector('.jumbotron');
 const footer = document.querySelector('footer');
 
 // Style :
@@ -20,14 +20,27 @@ const goTo = e => { // we scroll smoothly to the section of the link we clicked
 }
 const activeLinkOnScroll = () => { // when we scroll over a section, the link in the navbar become active
     let scrollPos = window.scrollY + (header.offsetHeight + (rootFontSize * 2));
+
+    scrollPos <= heroImg.offsetHeight + header.offsetHeight ? 
+            navLinks[0].classList.add('active') : 
+            navLinks[0].classList.remove('active');
+
+   
+
     navLinks.forEach(link => {
+
         let sectionHash = document.querySelector(link.hash);
-        if (scrollPos === 0) navLinks[0].classList.add('active');
+
         (scrollPos >= sectionHash.offsetTop && scrollPos <= (sectionHash.offsetTop + sectionHash.offsetHeight)) ? 
             link.classList.add('active') : 
             link.classList.remove('active');
-        if (scrollPos >= footer.offsetTop) navLinks[--navLinks.length].classList.add('active');
-    })
+    });
+
+    if ((scrollPos + window.innerHeight / 2.5 + footer.offsetHeight) >= (footer.offsetTop)) {
+        navLinks[--navLinks.length].classList.add('active');
+        navLinks[navLinks.length - 2].classList.remove('active');
+    } else
+        navLinks[--navLinks.length].classList.remove('active');
 }
 
 // Event listeners :
